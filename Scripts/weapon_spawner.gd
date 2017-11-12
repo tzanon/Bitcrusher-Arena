36b7a9e2a176b05015464d3bf6c115bfe9e46064
Tyler Zanon <tdzanon@gmail.com>
2017-11-12 00:26:12 -0500

    fixed random weapon spawning

--- Scripts/weapon_spawner.gd
+++ Scripts/weapon_spawner.gd
@@ -8,19 +8,17 @@ const weapon_scenes = [
 	preload("res://Scenes/Weapons/world_blue_ph.tscn")
 ] #world weapon scenes to instantiate from
 
-
-#TODO: exported enum and case statement to select spawn mode
-
 const light_weapon_scenes = []
 const power_weapon_scenes = [] #more powerful: spawned in harder places
 
-var total_spawn_positions #array of vector2
+#TODO: exported enum and case statement to select spawn mode
 
+var total_spawn_positions #array of vector2
 export(Dictionary) var position_weapon_scenes
 export(Dictionary) var position_possible_weapon_scenes
 
-export var MIN_SPAWN = 0
-export var MAX_SPAWN = 10
+var MIN_SPAWN
+var MAX_SPAWN
 
 var arena_weapon_refs #array of all weapons currently in the arena
 
@@ -37,23 +35,25 @@ func _ready():
 			child.hide()
 			total_spawn_positions.append(child.get_global_pos())
 	
+	MIN_SPAWN = 0
+	MAX_SPAWN = total_spawn_positions.size()
+	
 	arena_weapon_refs = []
-	_spawn_static_random()
+	_spawn_select_random()
 	
 	set_process(true)
 
 func _process(delta):
 	if refresh_timer.get_time_left() <= 0:
-		print("refreshing weapons!")
 		_clear_weapons()
-		_spawn_static_random()
+		_spawn_select_random()
 		refresh_timer.start()
 	
 
 func _clear_weapons():
+	print("clearing ", arena_weapon_refs.size(), " weapons")
 	for weapon_ref in arena_weapon_refs:
 		if weapon_ref.get_ref():
-			print("weapon is ", weapon_ref.get_ref())
 			weapon_ref.get_ref().queue_free()
 	
 	arena_weapon_refs.clear()
@@ -76,14 +76,14 @@ func _spawn_static_random():
 # randomly selects positions and spawns the corresponding weapon at each
 func _spawn_select_fixed():
 	var spawn_positions = select_spawn_positions()
-	for pos in spawn_positions():
+	for pos in spawn_positions:
 		var weapon = _spawn_weapon_with_position(pos)
 		arena_weapon_refs.append(weakref(weapon))
 
 # randomly selects positions and spawns a random weapon at each
 func _spawn_select_random():
-	var spawn_positions = select_spawn_positions()
-	for pos in total_spawn_positions:
+	var spawn_positions = _select_spawn_positions()
+	for pos in spawn_positions:
 		var weapon = _spawn_random_weapon(pos)
 		arena_weapon_refs.append(weakref(weapon))
 
@@ -94,10 +94,13 @@ func _select_spawn_positions():
 	var num_weapons = randi() % (MAX_SPAWN - MIN_SPAWN + 1) + MIN_SPAWN
 	var spawn_positions = []
 	var possible_positions = [] + total_spawn_positions
+	
 	for i in range(num_weapons):
-		var j = random(0, possible_positions.size())
+		print("i is ", i)
+		var j = randi() % (possible_positions.size())
 		spawn_positions.append(possible_positions[j])
 		possible_positions.remove(j)
+	
 	return spawn_positions
 
 
@@ -107,7 +110,7 @@ func _spawn_random_weapon(pos):
 	var i = randi() % (weapon_scenes.size())
 	var weapon = weapon_scenes[i].instance()
 	weapon.set_global_pos(pos)
-	print("spawned weapon ", weapon)
+	print("spawned weapon ", weapon.get_name())
 	#get_parent().add_child(weapon)
 	#get_tree().get_root().add_child(weapon)
 	get_tree().get_root().call_deferred("add_child", weapon)
