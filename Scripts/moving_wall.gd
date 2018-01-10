bb181998deeb67d7d893835366c31d6e59e1fd04
nigh7sh4de <plotnikd@gmail.com>
2018-01-09 20:35:37 -0500

    Added basic level design

---
+++ Scripts/moving_wall.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+export (float) var period = 1
+export (int) var dx = 0
+export (int) var dy = 0
+export (bool) var paused
+
+var initial_pos
+var time = 0
+var reverse = false
+
+func _ready():
+	print(paused)
+	initial_pos = get_pos()
+	set_process(true)
+
+func _process(delta):
+	time += delta
+	if !paused:
+		var _dx = (time / period) * dx
+		var _dy = (time / period) * dy
+		if reverse: 
+			_dx = dx - _dx
+			_dy = dy - _dy
+		set_pos(Vector2(initial_pos.x + _dx, initial_pos.y + _dy))
+	
+	if time > period:
+		time = 0
+		paused = !paused
+		if paused: reverse = !reverse
+
