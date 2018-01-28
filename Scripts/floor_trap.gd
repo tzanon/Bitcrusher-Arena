0ad6e10de8e2db37d234e0e41c2c2a914681f492
nigh7sh4de <plotnikd@gmail.com>
2018-01-28 17:09:33 -0500

    Fix floor trap behaviour

--- Scripts/floor_trap.gd
+++ Scripts/floor_trap.gd
@@ -1,29 +1,41 @@
 extends Area2D
 
-export (int) var damage_amount = 0
-export (float) var period = 1
+export (float) var period = 1.0
+export (int, 1, 100) var damage_amount = 1
+
+var paused = false
 
 var off_texture = preload("res://Sprites/trap_off.png")
 var on_texture = preload("res://Sprites/trap_on.png")
 var sprite
+var pause_timer
+var damage_timer
 
-var time = 0
-var paused = false
 
 func _ready():
 	sprite = get_node("Sprite")
+	pause_timer = Timer.new()
+	damage_timer = Timer.new()
+	add_child(pause_timer)
+	add_child(damage_timer)
+	pause_timer.set_wait_time(period)
+	damage_timer.set_wait_time(float(period) / (damage_amount + 1))
+	pause_timer.connect("timeout", self, "_interval")
+	damage_timer.connect("timeout", self, "_damage")
+	pause_timer.start()
 	set_fixed_process(true)
+	
 
-func _fixed_process(delta):
-	time += delta
-	if time > period:
-		time = 0
-		paused = !paused
-		if paused: sprite.set_texture(off_texture)
-		if !paused: sprite.set_texture(on_texture)
+func _interval():
+	paused = !paused
+	if paused: sprite.set_texture(off_texture)
+		damage_timer.stop()
+	if !paused: 
+		sprite.set_texture(on_texture)
+		damage_timer.start()
 
-	if !paused:
-		var bodies = get_overlapping_bodies()
-		if bodies.size() > 0:
-			if bodies[0].is_in_group("Damageable"):
-				bodies[0].damage(damage_amount * delta)
+func _damage():
+	var bodies = get_overlapping_bodies()
+	if bodies.size() > 0:
+		if bodies[0].is_in_group("Damageable"):
+			bodies[0].damage(1)
